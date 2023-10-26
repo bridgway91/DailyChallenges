e71df8c643892f0b61b3c7687bc8741eca1c9018
Brandon Ridgway <bridgway91@gmail.com>
2023-10-25 23:22:21 -0400

    10/25/23 challenge - been a while since i did a challenge, another easy one, might raise difficulty after a couple more, but was relatively busy today so no API work done prior to midnight

---
+++ Codewars/Find_Middle.js
@@ -0,0 +1,27 @@
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+function gimme (triplet) { // not too difficult, just had to do some testing in console to be sure of '...' syntax, plus making sure the logic of the inner function was correct
+    return triplet.findIndex(el => el != Math.max(...triplet) && el != Math.min(...triplet))
+}
+
+// alternatively...
+
+function gimme(a) { // forgot that sort was a thing...
+    return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+  }
+
+const gimme = function (arr) {
+    return arr.indexOf([...arr].sort((x, y) => x > y)[1]);
+  };
