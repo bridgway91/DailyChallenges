923a560f5b71131a05b43e9a755e21b1ee564680
Brandon Ridgway <bridgway91@gmail.com>
2024-03-01 09:55:03 -0500

    3/1/24 challenge

---
+++ Codewars/K6/K6_Salesmans_Travel.js
@@ -0,0 +1,58 @@
+// A traveling salesman has to visit clients. He got each client's address e.g. "432 Main Long Road St. Louisville OH 43071" as a list.
+
+// The basic zipcode format usually consists of two capital letters followed by a white space and five digits. The list of clients to visit was given as a string of all addresses, each separated from the others by a comma, e.g. :
+// "123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432".
+// To ease his travel he wants to group the list by zipcode.
+
+// Task
+// The function travel will take two parameters r (addresses' list of all clients' as a string) and zipcode and returns a string in the following format:
+// zipcode:street and town,street and town,.../house number,house number,...
+// The street numbers must be in the same order as the streets where they belong.
+// If a given zipcode doesn't exist in the list of clients' addresses return "zipcode:/"
+
+// Examples
+// r = "123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432"
+// travel(r, "OH 43071") --> "OH 43071:Main Street St. Louisville,Main Long Road St. Louisville/123,432"
+// travel(r, "NY 56432") --> "NY 56432:High Street Pollocksville/786"
+// travel(r, "NY 5643") --> "NY 5643:/"
+
+// Note:
+// You can see a few addresses and zipcodes in the test cases.
+
+
+function travel(r, zipcode) { // the exceptions were catching me for a bit, unsure if there was an easier way to handle those
+    if (!r.includes(zipcode)) return `${zipcode}:/`
+    if (!zipcode) return `:/`
+    const houses = r.split(',')
+    let st = []
+    let n = []
+    for (let h of houses) {
+      if (h.includes(zipcode) && zipcode.length == 8) {
+        n.push(h.match(/[0-9]+/g)[0])
+        st.push(h.replace(zipcode,'').replace(n[n.length-1],'').trim())
+      }
+    }
+    return `${zipcode}:${st.join(',')}/${n.join(',')}`
+  }
+
+
+// alternatively...
+
+
+function travel(r, zipcode) { // i like this method, also got around issue of match and replacing text... substring() exists! im dumb
+    const addressList = r.split(",");
+    const streets = [];
+    const numbers = [];
+
+    addressList.forEach(address => {
+        let zip = address.substring(address.length - 8, address.length);
+        if (zip === zipcode) {
+            let number = address.substring(0, address.indexOf(" ") + 1);
+            let street = address.substring(address.indexOf(" "), address.length -8);
+            
+            streets.push(street.trim());
+            numbers.push(number.trim());
+        }
+    });
+    return `${zipcode}:${streets.join(",")}/${numbers.join(",")}`;
+}
