b1a38c223462f8729ce95caf00e1d852331dc987
Brandon Ridgway <bridgway91@gmail.com>
2024-03-07 03:41:42 -0500

    3/7/24 challenge

---
+++ Codewars/K6/K6_To_Prime.js
@@ -0,0 +1,41 @@
+// Task :
+// Given a List [] of n integers , find minimum number to be inserted in a list, so that sum of all elements of list should equal the closest prime number .
+
+// Notes
+// List size is at least 2 .
+// List's numbers will only positives (n > 0) .
+// Repetition of numbers in the list could occur .
+// The newer list's sum should equal the closest prime number .
+
+// Input >> Output Examples
+
+// 1- minimumNumber ({3,1,2}) ==> return (1)
+// Explanation:
+// Since , the sum of the list's elements equal to (6) , the minimum number to be inserted to transform the sum to prime number is (1) , which will make the sum of the List equal the closest prime number (7) .
+
+// 2-  minimumNumber ({2,12,8,4,6}) ==> return (5)
+// Explanation:
+// Since , the sum of the list's elements equal to (32) , the minimum number to be inserted to transform the sum to prime number is (5) , which will make the sum of the List equal the closest prime number (37) .
+
+// 3- minimumNumber ({50,39,49,6,17,28}) ==> return (2)
+// Explanation:
+// Since , the sum of the list's elements equal to (189) , the minimum number to be inserted to transform the sum to prime number is (2) , which will make the sum of the List equal the closest prime number (191) .
+
+
+
+function minimumNumber(numbers){ // so i totally copy+pasted the isPrime() function from stackoverflow, feel like that should be allowed, right?
+    let numSum = numbers.reduce((a,b)=>a+b,0)
+    let n = 0
+    while(!isPrime(numSum+n)) {
+      n++
+    }
+    return n
+  }
+const isPrime = num => {
+      for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
+          if(num % i === 0) return false;
+      }
+      return num > 1;
+  }
+
+// others basically did same method
