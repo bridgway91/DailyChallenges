037a474b2a6b8e124c8f764d13f2aa3842261632
Brandon Ridgway <bridgway91@gmail.com>
2024-07-02 16:39:12 -0400

    7/2/24 challenge

---
+++ Codewars/K6/K6_Ease_Stockbroker.js
@@ -0,0 +1,48 @@
+// Clients place orders to a stockbroker as strings. The order can be simple or multiple or empty.
+// Type of a simple order: Quote/white-space/Quantity/white-space/Price/white-space/Status
+// where Quote is formed of non-whitespace character, Quantity is an int, Price a double (with mandatory decimal point "." ), Status is represented by the letter B (buy) or the letter S (sell).
+
+// Example: "GOOG 300 542.0 B"
+// A multiple order is the concatenation of simple orders with a comma between each.
+// Example:
+// "ZNGA 1300 2.66 B, CLH15.NYM 50 56.32 B, OWW 1000 11.623 B, OGG 20 580.1 B"
+// or
+// "ZNGA 1300 2.66 B,CLH15.NYM 50 56.32 B,OWW 1000 11.623 B,OGG 20 580.1 B"
+
+// To ease the stockbroker your task is to produce a string of type
+// "Buy: b Sell: s" where b and s are 'double' formatted with no decimal, b representing the total price of bought stocks and s the total price of sold stocks. Example: "Buy: 294990 Sell: 0"
+
+// Unfortunately sometimes clients make mistakes. When you find mistakes in orders, you must pinpoint these badly formed orders and produce a string of type:
+// "Buy: b Sell: s; Badly formed nb: badly-formed 1st simple order ;badly-formed nth simple order ;"
+// where nb is the number of badly formed simple orders, b representing the total price of bought stocks with correct simple order and s the total price of sold stocks with correct simple order.
+// Examples:
+// "Buy: 263 Sell: 11802; Badly formed 2: CLH16.NYM 50 56 S ;OWW 1000 11 S ;"
+// "Buy: 100 Sell: 56041; Badly formed 1: ZNGA 1300 2.66 ;"
+
+// Notes:
+// - If the order is empty, Buy is 0 and Sell is 0 hence the return is: "Buy: 0 Sell: 0".
+// - Due to Codewars whitespace differences will not always show up in test results.
+// - With Golang (and maybe others) you can use a format with "%.0f" for "Buy" and "Sell".
+
+
+function balanceStatements(list){ // while being the only solution i didnt need to look at the answers for during this coding session, it was still extremely annoying trying to match up string representations
+    if (list.length == 0) return "Buy: 0 Sell: 0"
+    let buy=0, sell=0, bad=[]
+    const stocks = list.split(',').map(e=>e.trim().split(' '))
+  
+    for (let stock of stocks) {
+      if (stock[1].includes('.') || !stock[2].includes('.') || stock.length != 4) {
+        bad.push(stock)
+      } else {
+        if (stock[3] == 'B') {
+          buy += (Number(stock[1]) * Number(stock[2]))
+        } else {
+          sell += (Number(stock[1]) * Number(stock[2]))
+        }
+      }
+    }
+    
+    bad = bad.length ? '; ' + `Badly formed ${bad.length}: ` + bad.map(e=>e.join(' ') + ' ;').join('') : ''
+  
+    return `Buy: ${Math.round(buy)} Sell: ${Math.round(sell)}${bad}`
+  }
