5ca1d03e862c73647676160e8c38b02945b84fcc
Brandon Ridgway <bridgway91@gmail.com>
2024-07-15 10:52:28 -0400

    7/15/24 challenge

---
+++ Codewars/K6/K6_Implementing_ObjectCreate.js
@@ -0,0 +1,49 @@
+// BRIEFING
+// Greetings, warrior! In this kata you will learn, how Object.create method does its job.
+// First over, what does it do? The signature of Object.create is like this:
+//   //throws TypeError
+//   //returns newly created object
+//   Object.create(prototype, [properties])
+
+// Object.create serves to create new object that inherits given prototype and has properties being defined by "properties" parameter set on it. Parameters values must meet following requirements:
+// - prototype parameter should be some object1 (i.e. a non-primitive value) or null (but not undefined!). If bad value passed for prototype, error TypeError must be thrown.
+// - properties parameter can be of any type or omitted at all.
+
+// As a result of execution, Object.create will return a newly created object with inner property [[Prototype]]2 set to value of prototype parameter. If properties parameter is passed and is not undefined, then Object.create will make call Object.defineProperties(obj,properties), where obj is the object to be returned by Object.create.
+
+// OBJECTIVES
+// In absence of the original Object.create create your own implementation of it that acts like the original one and assign it to the Object.create. To access/modify inner [[Prototype]] property of an object, use the methods Object.getPrototypeOf() and Object.setPrototypeOf(), respectively. 2
+
+// Your implementation will be used like this:
+//   var citizen = {
+//     sleep: function(){ return "zzZ..."; },
+//     panic: function(){ return "AaAaAaAa!"; }
+//   };
+//   var veteran = Object.create(citizen,{
+//           panic: {
+//             value: function(){
+//               return "SNAFU";
+//             }
+//           }
+//         });
+
+// Notes
+// 1 In JavaScript everything is an object, except these primitives:
+//   - null
+//   - undefined
+//   - numbers (42, 3.14, -1, NaN, Infinity, Number('123'), etc.)
+//   - big Integers (-1n, 0n, 1n, BigInt(123), etc.)
+//   - strings ("", "dan", "abnormal termination", String(123), etc.)
+//   - boolean (true, false, Boolean(123))
+//   - symbols (Symbol("abc"), Symbol())
+//   Everything else, like RegExp literals (/abc/), functions, values created by invoking a function with new, Arrays, ... are objects.
+// 2 An inner property is a property is not accessible through language constructs. But [[Prototype]] is an exception and is acessible through Object.setPrototypeOf() and Object.getPrototypeOf().
+
+
+Object.create = function(prototype, properties = {}) { // ok.. so i got the first two lines of it on my own, then the rest of it i was kinda halfway there, but there were issues, tried to actually utilize both getPrototypeOf and setPrototypeOf, but most solns didnt bother with the former
+    if (typeof prototype !== 'object') throw new TypeError()
+    const obj = {}
+    Object.setPrototypeOf(obj,prototype)
+    Object.defineProperties(obj,properties)
+    return obj
+  };
