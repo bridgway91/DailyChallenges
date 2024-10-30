34a6586fbe459f63fdbf194400ba015b3fe597fc
Brandon Ridgway <bridgway91@gmail.com>
2024-10-29 21:05:32 -0400

    10/29/24 challenge

---
+++ Codewars/K6/K6_String_Words.js
@@ -0,0 +1,28 @@
+// You're given a string containing a sequence of words separated with whitespaces. Let's say it is a sequence of patterns: a name and a corresponding number - like this:
+// "red 1 yellow 2 black 3 white 4"
+
+// You want to turn it into a different string of objects you plan to work with later on - like this:
+// "[{name : 'red', id : '1'}, {name : 'yellow', id : '2'}, {name : 'black', id : '3'}, {name : 'white', id : '4'}]"
+
+// Doing this manually is a pain. So you've decided to write a short function that would make the computer do the job for you. Keep in mind, the pattern isn't necessarily a word and a number. Consider anything separeted by a whitespace, just don't forget: an array of objects with two elements: name and id.
+
+// As a result you'll have a string you may just copy-paste whenever you feel like defining a list of objects - now without the need to put in names, IDs, curly brackets, colon signs, screw up everything, fail searching for a typo and begin anew. This might come in handy with large lists.
+
+
+
+function wordsToObject(input) { // another dumb prob with misleading instructions... above seems to indicate wanting an array of objects, but NOPE! just purely string... so dumb
+    const seq = input.split(' ')
+    let arr = []
+    for (let i=0; i<seq.length; i+=2) {
+      arr.push(`{name : '${seq[i]}', id : '${seq[i+1]}'}`)
+    }
+    return '[' + arr.join(', ') + ']'
+  }
+
+
+// alternatively...
+
+
+function wordsToObject(input) { // nice to see that i understand regex enough to immediately see that this works... slightly annoyed i didnt think of it, but i was still hung up on trying to make it an array, so w/e
+    return "["+input.replace(/(\S+) (\S+)/g,"{name : '$1', id : '$2'},").slice(0,-1)+"]"
+  }
