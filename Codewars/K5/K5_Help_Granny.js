9b1eb323a64ccc70e3c103fa42bcac8547c42028
Brandon Ridgway <bridgway91@gmail.com>
2025-01-29 00:59:48 -0500

    1/29/25 challenge

---
+++ Codewars/K5/K5_Help_Granny.js
@@ -0,0 +1,31 @@
+// Your granny, who lives in town X0, has friends. These friends are given in an array, for example: array of friends is ["A1", "A2", "A3", "A4", "A5"].
+// The order of friends in this array must not be changed since this order gives the order in which they will be visited.
+// Friends inhabit towns and you get an array with friends and their towns (or an associative array), for example: [["A1", "X1"], ["A2", "X2"], ["A3", "X3"], ["A4", "X4"]] which means A1 is in town X1, A2 in town X2... It can happen that we do not know the town of one of the friends hence it will not be visited.
+
+// Your granny wants to visit her friends and to know approximately how many miles she will have to travel. You will make the circuit that permits her to visit her friends. For example here the circuit will be:X0, X1, X2, X3, X4, X0 and you will compute approximately the total distance X0X1 + X1X2 + .. + X4X0.
+// For the distances you are given an array or a dictionary that gives each distance X0X1, X0X2 and so on. For example (it depends on the language):
+// [ ["X1", 100.0], ["X2", 200.0], ["X3", 250.0], ["X4", 300.0] ]
+// which means that X1 is at 100.0 miles from X0, X2 at 200.0 miles from X0, etc... It's not real life, it's a story... : the towns X0, X1, .., X0 are placed in the following manner (see drawing below):
+// X0X1X2 is a right triangle with the right angle in X1, X0X2X3 is a right triangle with the right angle in X2, ... In a travel X0, X1, .., Xi-1, Xi, Xi+1.., X0 you will suppose - to make it easier - that there is a right angle in Xi (i > 0).
+// So if a town Xi is not visited you will consider that the triangle  X0Xi-1Xi+1 is still a right triangle in Xi-1 and you can use the "Pythagorean_theorem".
+
+// Task
+// Can you help your granny and give her approximately the distance to travel?
+
+
+function tour(friends, fTowns, distTable) { // not too hard, just a little annoying with how the inputs were handled
+    const o = {}
+    for (let f of fTowns) {
+      let d = distTable[distTable.indexOf(f[1])+1]
+      o[f[0]] = [f[1],d]
+    }
+  
+    let t = friends.filter(e=>o[e]).map((e,i,a)=>{
+      if(i==0) return o[e][1]
+      let a2 = Math.pow(o[friends[i-1]][1],2)
+      let c2 = Math.pow(o[e][1],2)
+      return i == a.length-1 ? Math.sqrt(c2-a2) + o[e][1] : Math.sqrt(c2-a2)
+    })
+  
+    return Math.floor(t.reduce((a,b)=>a+b))
+  }
