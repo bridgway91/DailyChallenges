d8b3509be79f569fd061af72d9a826d0982286f8
Brandon Ridgway <bridgway91@gmail.com>
2025-08-14 18:22:34 -0400

    8/14/25 challenge

---
+++ Codewars/August_25/Mathematics/K6_Missing_Letter.js
@@ -0,0 +1,17 @@
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+// (Use the English alphabet with 26 letters!)
+
+
+function findMissingLetter(array){ // simple, but wonder if theres a slimmer method...
+  const s = array[0].charCodeAt(0)
+  for(let i=1; i<array.length; i++) {
+    if(array[i].charCodeAt(0) !== s+i) {return String.fromCharCode(s+i)}
+  }
+  return null
+}
