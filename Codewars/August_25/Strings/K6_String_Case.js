7e7225e67a7d38cb5e5922b79cdc7b35e31d553e
Brandon Ridgway <bridgway91@gmail.com>
2025-08-26 01:12:51 -0400

    8/26/25 challenge

---
+++ Codewars/August_25/Strings/K6_String_Case.js
@@ -0,0 +1,11 @@
+// Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased and you need to start over for each word.
+// The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+// Examples:
+// "String" => "StRiNg"
+// "Weird string case" => "WeIrD StRiNg CaSe"
+
+
+function toWeirdCase(string){ // APPARENTLY index is by word, not total index
+  return string.split(' ').map(w=>w.split('').map((e,i)=>i%2?e.toLowerCase():e.toUpperCase()).join('')).join(' ')
+}
